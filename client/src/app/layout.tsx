ad8f5b172a0af8248e936eab04c7e9471c3b7cd0
doheeMac <thsehgml37@korea.ac.kr>
2024-07-13 23:30:08 +0900

    chore: add head tag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="en">
+         <head></head>
          <body className={inter.className}>
             <ReactQueryClientProviders>
                <AppRouterCacheProvider
