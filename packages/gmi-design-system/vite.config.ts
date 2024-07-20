317c800d7174d62d5f9754dce136458b0a697b10
doheeMac <thsehgml37@korea.ac.kr>
2024-07-20 17:56:54 +0900

    chore: fix config with ve

--- packages/gmi-design-system/vite.config.ts
+++ packages/gmi-design-system/vite.config.ts
@@ -1,55 +1,56 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-
-import dts from "vite-plugin-dts";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "./src/index.ts"),
-      name: "cds",
-      fileName: "index",
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'cds',
+      fileName: 'index',
     },
     sourcemap: true,
     emptyOutDir: true,
     rollupOptions: {
       external: [
-        "react",
-        "react-dom",
-        "@emotion/react",
-        "@emotion/styled",
-        "@emotion/react/jsx-runtime",
+        'react',
+        'react-dom',
+        '@emotion/react',
+        '@emotion/styled',
+        '@emotion/react/jsx-runtime',
       ],
       output: [
         {
-          format: "cjs",
-          preserveModulesRoot: "src",
-          entryFileNames: "[name].js",
+          format: 'cjs',
+          preserveModulesRoot: 'src',
+          entryFileNames: '[name].js',
         },
         {
-          format: "es",
+          format: 'es',
           preserveModules: true,
-          preserveModulesRoot: "src",
-          entryFileNames: "[name].mjs",
+          preserveModulesRoot: 'src',
+          entryFileNames: '[name].mjs',
         },
       ],
     },
   },
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
-      jsxRuntime: "automatic",
+      jsxImportSource: '@emotion/react',
+      jsxRuntime: 'automatic',
     }),
-    dts({ tsconfigPath: "./tsconfig.build.json" }),
+    dts({ tsconfigPath: './tsconfig.build.json' }),
     viteStaticCopy({
       targets: [
         {
-          src: ["src/*.css"],
-          dest: resolve(__dirname, "dist"),
+          src: ['src/*.css'],
+          dest: resolve(__dirname, 'dist'),
         },
       ],
     }),
+    vanillaExtractPlugin(),
   ],
-});
+})
