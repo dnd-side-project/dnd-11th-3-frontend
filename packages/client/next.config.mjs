1e86f917c0aba82d9268873186f25ccf775d5acc
doheeMac <thsehgml37@korea.ac.kr>
2024-07-20 17:55:17 +0900

    chore: set config with ve

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
+const withVanillaExtract = createVanillaExtractPlugin()
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = withVanillaExtract(nextConfig)
