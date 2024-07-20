e4d7e97d8ed664c6fab9d95dae70330e38cd619f
doheeMac <thsehgml37@korea.ac.kr>
2024-07-20 18:11:37 +0900

    fix: fix name to gmi

--- packages/gmi-design-system/vite.config.ts
+++ packages/gmi-design-system/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'cds',
+      name: 'gmi-design-system',
       fileName: 'index',
     },
     sourcemap: true,
