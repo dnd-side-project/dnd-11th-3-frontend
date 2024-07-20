fde2a7c97eb157d4cb253b3edba2d37b4bbc8e46
doheeMac <thsehgml37@korea.ac.kr>
2024-07-20 17:21:25 +0900

    chore: add setting for gmi design system

---
+++ packages/gmi-design-system/vite.config.ts
@@ -0,0 +1,55 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "cds",
+      fileName: "index",
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "@emotion/react",
+        "@emotion/styled",
+        "@emotion/react/jsx-runtime",
+      ],
+      output: [
+        {
+          format: "cjs",
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].js",
+        },
+        {
+          format: "es",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].mjs",
+        },
+      ],
+    },
+  },
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      jsxRuntime: "automatic",
+    }),
+    dts({ tsconfigPath: "./tsconfig.build.json" }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: ["src/*.css"],
+          dest: resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
+});
