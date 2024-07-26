3655b2ecc37d142d89ed2d432853207e34c1c2b8
SuJin <kimsujin001127@gmail.com>
2024-07-26 15:53:07 +0900

    test: husky test

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+   console.log('test')
    return (
       <main className={styles.main}>
          <div className={styles.description}>
