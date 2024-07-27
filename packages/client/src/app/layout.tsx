d7cb936a95b041a0770141c99b42d59ad1a32d76
SuJin <kimsujin001127@gmail.com>
2024-07-27 12:52:20 +0900

    remove: 콘솔 삭제

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
 }: Readonly<{
    children: React.ReactNode
 }>) {
-   console.log('test')
    return (
       <html lang="en">
          <head></head>
--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-   console.log('test')
    return (
       <main className={styles.main}>
          <div className={styles.description}>
