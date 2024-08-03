9d37cb709f4795010e0039d67c68523811559d7a
doheeMac <thsehgml37@korea.ac.kr>
2024-08-04 00:11:07 +0900

    fix: fix vite config

--- packages/gmi-design-system/vite.config.ts
+++ packages/gmi-design-system/vite.config.ts
@@ -5,10 +5,16 @@ import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import config from './.storybook/main'
+
+// tsconfg setting is not working
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import packageJson from './package.json'
+
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, './src/index.ts'),
+      entry: convertExportsToEntries(packageJson.exports),
       name: 'gmi-design-system',
       fileName: 'index',
     },
@@ -47,3 +53,14 @@ export default defineConfig({
     vanillaExtractPlugin(config),
   ],
 })
+
+function convertExportsToEntries(exports: object) {
+  const entries: Record<string, string> = {}
+  for (const key in exports) {
+    if (/^\.\S+[^/]+\.[^/]+$/.test(key)) continue // Ignore regular expression patterns that end with *.*
+    const entryPath = key === '.' ? './src/index.ts' : `src/${key}/index.ts`
+    const formattedKey = key === '.' ? 'index' : `${key.slice(2)}/index`
+    entries[formattedKey] = resolve(__dirname, entryPath)
+  }
+  return entries
+}
