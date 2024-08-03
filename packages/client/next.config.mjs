a556d0851b48d7824f52f5664bb3d308cf7b7cc5
doheeMac <thsehgml37@korea.ac.kr>
2024-08-03 19:33:55 +0900

    chore: fix cmd and config

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -4,6 +4,10 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+   eslint: {
+      ignoreDuringBuilds: true,
+   },
+}
 
 export default withVanillaExtract(nextConfig)
