63c72169e57de066337193021462737172b9c852
doheeMac <thsehgml37@korea.ac.kr>
2024-08-07 00:50:39 +0900

    fix: fix module not found err

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+import path from 'path'
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
+   webpack: (config) => {
+      config.resolve.alias['@gmi-design-system'] = path.resolve(
+         __dirname,
+         '../gmi-design-system/src',
+      )
+      return config
+   },
 }
 
 export default withVanillaExtract(nextConfig)
