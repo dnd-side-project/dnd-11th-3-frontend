ea900415a3ce4cbafb5e01fd2dca379fd4ed648f
doheeMac <thsehgml37@korea.ac.kr>
2024-08-07 00:26:00 +0900

    fix: fix config and eslint

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,3 +1,9 @@
+import { COLOR } from '@gmi-design-system/src'
+
 export default function Signin() {
-   return <main>Signin(로그인 페이지)</main>
+   return (
+      <main style={{ backgroundColor: COLOR.primary }}>
+         Signin(로그인 페이지)
+      </main>
+   )
 }
