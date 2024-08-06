84ae90c3a4d0e05f25bf5463b9b02f14e012186b
doheeMac <thsehgml37@korea.ac.kr>
2024-08-07 00:31:12 +0900

    chore: test commit for deploy

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -3,7 +3,7 @@ import { COLOR } from '@gmi-design-system/src'
 export default function Signin() {
    return (
       <main style={{ backgroundColor: COLOR.primary }}>
-         Signin(로그인 페이지)
+         Signin(로그인 페이지) test
       </main>
    )
 }
