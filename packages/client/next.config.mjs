031a23c3d0737cf9a502d02829363e919a329c11
doheeMac <thsehgml37@korea.ac.kr>
2024-08-07 00:53:35 +0900

    fix: dirname not defined err

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+// ES 모듈에서 __dirname 대체
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
