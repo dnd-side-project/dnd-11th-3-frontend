85c8e6d7b2a488d4039114ba26797ea2af10faa6
dohui-son <thsehgml37@korea.ac.kr>
2024-08-10 11:45:54 +0900

    fix: fix name (#27)

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,9 +1,3 @@
-import { COLOR } from '@gmi-design-system'
-
 export default function Signin() {
-   return (
-      <main style={{ backgroundColor: COLOR.primary }}>
-         Signin(로그인 페이지) test
-      </main>
-   )
+   return <main>Signin(로그인 페이지) test</main>
 }
