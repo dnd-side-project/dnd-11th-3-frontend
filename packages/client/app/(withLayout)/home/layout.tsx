92585390816522dc695db8d0a2af73982d81be8a
doheeMac <thsehgml37@korea.ac.kr>
2024-08-10 23:08:01 +0900

    fix: fix authlayout

--- packages/client/app/(withLayout)/home/layout.tsx
+++ packages/client/app/(withLayout)/home/layout.tsx
@@ -1,24 +1,10 @@
-'use client'
-
-import React, { useMemo } from 'react'
-import { usePathname } from 'next/navigation'
+import React from 'react'
 import { AuthLayout } from '../../../src/apps/layout'
-import { WITHOUT_COMMON_HEADER } from '../../../src/shared/model/navMetaData'
 
 interface Props {
    children: React.ReactNode
 }
 
 export default function Layout({ children }: Props) {
-   const pathname = usePathname()
-
-   const showHeader = useMemo(
-      () => !WITHOUT_COMMON_HEADER.includes(pathname),
-      [pathname],
-   )
-   return (
-      <AuthLayout showHeader={showHeader} pageTitle="질문 목록">
-         {children}
-      </AuthLayout>
-   )
+   return <AuthLayout>{children}</AuthLayout>
 }
--- packages/client/src/apps/layout/AuthLayout.tsx
+++ packages/client/src/apps/layout/AuthLayout.tsx
@@ -1,18 +1,25 @@
 'use client'
 
-import React from 'react'
+import React, { useMemo } from 'react'
+import { usePathname } from 'next/navigation'
+import { WITHOUT_COMMON_HEADER } from '@shared/model/navMetaData'
 import { Header } from './Header'
 
 interface Props {
    children: React.ReactNode
-   showHeader?: boolean
-   pageTitle?: string
 }
 
-export function AuthLayout({ children, showHeader = true, pageTitle }: Props) {
+export function AuthLayout({ children }: Props) {
+   const pathname = usePathname()
+
+   const showHeader = useMemo(
+      () => !WITHOUT_COMMON_HEADER.includes(pathname),
+      [pathname],
+   )
+
    return (
       <div>
-         {showHeader && <Header title={pageTitle} />}
+         {showHeader && <Header title="TODO 타이틀 상수 이용" />}
          {children}
          {/* <BottomNavigation /> */}
       </div>
