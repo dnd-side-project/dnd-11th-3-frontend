1935d5121075892b1d4a5c39e4561d9b1f147217
SuJin <kimsujin001127@gmail.com>
2024-08-11 22:23:48 +0900

    feat: 추천질문 캐러셀 추가
    react-slick 라이브러리 사용
    npm install 필요

--- packages/client/app/(withLayout)/home/index.css.ts
+++ packages/client/app/(withLayout)/home/index.css.ts
@@ -3,4 +3,34 @@ import { style } from '@vanilla-extract/css'
 export const HomeWrapper = style({
    display: 'flex',
    flexDirection: 'column',
+   padding: '0 16px',
+})
+
+export const RecommendWrapper = style({
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   justifyContent: 'center',
+})
+
+export const RecommendHeaderBox = style({
+   display: 'flex',
+   width: '100%',
+})
+
+export const RecommendTitle = style({
+   marginRight: 'auto',
+})
+export const MoreBox = style({
+   marginLeft: 'auto',
+   width: '52px',
+})
+
+export const RecommendContentBox = style({
+   width: '100%',
+   marginTop: '10px',
+})
+
+export const RecommendItem = style({
+   padding: '0 16px',
 })
--- packages/client/app/(withLayout)/home/page.tsx
+++ packages/client/app/(withLayout)/home/page.tsx
@@ -1,11 +1,73 @@
+'use client'
+
 import { HomeHeader } from '@widgets/HomeHeader/HomeHeader'
-import { HomeWrapper } from './index.css'
+import { Button } from '@gmi-design-system/component/Button/Button'
+import React from 'react'
+import dynamic from 'next/dynamic'
+import Green from '@gmi-design-system/component/Recommend/home/Green.svg'
+import Blue from '@gmi-design-system/component/Recommend/home/Blue.svg'
+import Orange from '@gmi-design-system/component/Recommend/home/Orange.svg'
+import Image from 'next/image'
+import * as styles from './index.css'
+import { useClientSideRender } from './useClientSideRender'
+
+const Slider = dynamic(() => import('react-slick'), { ssr: false })
 
 export default function Home() {
+   const isClient = useClientSideRender()
+
+   const settings = {
+      className: 'center',
+      infinite: true,
+      centerPadding: '60px',
+      slidesToShow: 2,
+      swipeToSlide: true,
+   }
+
    return (
-      <div className={HomeWrapper}>
-         <HomeHeader />
-         <main />
-      </div>
+      <>
+         <div className={styles.HomeWrapper}>
+            <HomeHeader />
+            <div className={styles.RecommendWrapper}>
+               <div className={styles.RecommendHeaderBox}>
+                  <span className={styles.RecommendTitle}>추천 질문</span>
+                  <div className={styles.MoreBox}>
+                     <Button size="small" variant="outlined">
+                        더보기
+                     </Button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div className={styles.RecommendContentBox}>
+            {isClient && (
+               <div className="slider-container">
+                  <Slider
+                     className={settings.className}
+                     infinite={settings.infinite}
+                     centerPadding={settings.centerPadding}
+                     slidesToShow={settings.slidesToShow}
+                     swipeToSlide={settings.swipeToSlide}
+                  >
+                     <div className={styles.RecommendItem}>
+                        <Image src={Orange} alt="orange title" />
+                     </div>
+                     <div className={styles.RecommendItem}>
+                        <Image src={Blue} alt="orange title" />
+                     </div>
+                     <div className={styles.RecommendItem}>
+                        <Image src={Green} alt="orange title" />
+                     </div>
+                     <div className={styles.RecommendItem}>
+                        <Image src={Orange} alt="orange title" />
+                     </div>
+                     <div className={styles.RecommendItem}>
+                        <Image src={Green} alt="orange title" />
+                     </div>
+                  </Slider>
+               </div>
+            )}
+         </div>
+      </>
    )
 }
