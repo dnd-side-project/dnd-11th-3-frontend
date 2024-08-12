5ac3f104b0a5e9c25f7a4e5b57304c0833e01779
SuJin <kimsujin001127@gmail.com>
2024-08-13 00:25:28 +0900

    feat: handleOnChange 함수 추가

--- packages/client/app/(withLayout)/signup/page.tsx
+++ packages/client/app/(withLayout)/signup/page.tsx
@@ -3,9 +3,31 @@
 import { Button } from '@gmi-design-system/component/Button/Button'
 // import { Select } from '@gmi-design-system/component/Select/Select'
 // import { TextInput } from '@gmi-design-system/component/TextInput/TextInput'
+import { SignUpType } from '@entities/@types/signup'
+import { ChangeEvent, useState } from 'react'
 import * as styles from './index.css'
 
+export type EssentialDataType = SignUpType & {
+   authCode: string
+}
+
 export default function Signup() {
+   const [essentialData, setEssentialData] = useState<EssentialDataType>({
+      officialEmail: '',
+      nickname: '',
+      jobGroup: '',
+      jobCategory: '',
+      authCode: '',
+   })
+
+   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target
+      setEssentialData((prev) => ({
+         ...prev,
+         [name]: value,
+      }))
+   }
+
    return (
       <div className={styles.Wrapper}>
          <div className={styles.Title}>공무원 이메일 인증</div>
@@ -18,7 +40,11 @@ export default function Signup() {
                description="Description"
                placeholder="Placeholder"
             /> */}
-            <input />
+            <input
+               name="officialEmail"
+               value={essentialData.officialEmail}
+               onChange={handleOnChange}
+            />
             <Button size="small" variant="disabled">
                인증
             </Button>
@@ -29,7 +55,11 @@ export default function Signup() {
                description="Description"
                placeholder="Placeholder"
             /> */}
-            <input />
+            <input
+               name="authCode"
+               value={essentialData.authCode}
+               onChange={handleOnChange}
+            />{' '}
             <Button size="small" variant="disabled">
                확인
             </Button>
@@ -41,7 +71,11 @@ export default function Signup() {
                description="2 - 12자 이내로 입력해주세요"
                placeholder="Placeholder"
             /> */}
-            <input />
+            <input
+               name="nickname"
+               value={essentialData.nickname}
+               onChange={handleOnChange}
+            />{' '}
             <Button size="small" variant="disabled">
                중복확인
             </Button>
@@ -59,7 +93,17 @@ export default function Signup() {
             selected={selectedWithIcon}
             items={[{ label: 'Option 1', id: '1' }]}
          /> */}
+         <input
+            name="jobGroup"
+            value={essentialData.jobGroup}
+            onChange={handleOnChange}
+         />
          <div className={styles.Title}>직렬</div> {/** TODO: select */}
+         <input
+            name="jobCategory"
+            value={essentialData.jobCategory}
+            onChange={handleOnChange}
+         />
          <div className={styles.FinalBtnBox}>
             <Button type="button" size="medium" variant="disabled">
                가입하기
