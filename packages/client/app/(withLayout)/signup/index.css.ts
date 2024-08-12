05b2f7659e578cc53b3d3c665ab3251eb3e97654
SuJin <kimsujin001127@gmail.com>
2024-08-12 21:48:56 +0900

    style: 스타일 파일 추가

---
+++ packages/client/app/(withLayout)/signup/index.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css'
+
+export const Wrapper = style({
+   display: 'flex',
+   flexDirection: 'column',
+   padding: '16px',
+   height: '100%',
+})
+
+export const Title = style({
+   marginBottom: '18px',
+})
+
+export const InputBtnWrapper = style({
+   display: 'flex',
+   marginBottom: '36px',
+})
+
+export const NickNameBox = style({
+   display: 'flex',
+})
+
+export const FinalBtnBox = style({
+   marginTop: 'auto',
+})
