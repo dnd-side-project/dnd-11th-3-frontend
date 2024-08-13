6b7238b3d1d3b4254d51b98315c54b377516dbdc
SuJin <kimsujin001127@gmail.com>
2024-08-13 13:00:13 +0900

    feat: login 와이어프레임 제작

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,3 +1,28 @@
+import SNSLogin from '@gmi-design-system/component/SignIn/SNSLogin'
+import * as styles from './signin.css'
+
 export default function Signin() {
-   return <main>Signin(로그인 페이지) test</main>
+   return (
+      <div className={styles.Wrapper}>
+         <div className={styles.TextWrapper}>
+            <div className={styles.TitleTxt}>
+               <span>공무원 업무습득</span>
+               <span>Q&A 서비스</span>
+            </div>
+            <div className={styles.TitleTxt}>
+               <span>어렵고 헷갈렸던 업무를</span>
+               <span>이제 공무인과 함께</span>
+               <span>차근차근 해결해 나가요!</span>
+            </div>
+         </div>
+         <div className={styles.LoginBtnWrapper}>
+            <div>
+               <SNSLogin SNS="카카오" type="시작하기" />
+            </div>
+            <div style={{ marginTop: '16px' }}>
+               <SNSLogin SNS="네이버" type="시작하기" />
+            </div>
+         </div>
+      </div>
+   )
 }
---
+++ packages/client/app/signin.css.ts
@@ -0,0 +1,26 @@
+import { style } from '@vanilla-extract/css'
+
+export const Wrapper = style({
+   display: 'flex',
+   flexDirection: 'column',
+})
+
+export const TextWrapper = style({
+   display: 'flex',
+   flexDirection: 'column',
+   marginTop: '160px',
+   marginLeft: '28px',
+})
+
+export const TitleTxt = style({
+   marginBottom: '18px',
+   display: 'flex',
+   flexDirection: 'column',
+})
+
+export const LoginBtnWrapper = style({
+   flexDirection: 'column',
+   display: 'flex',
+   padding: '0 16px',
+   marginTop: '203px',
+})
