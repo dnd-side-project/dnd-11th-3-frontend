4ce7a8d9bac81e7efd6837ff17b1d875c26b18a7
SuJin <kimsujin001127@gmail.com>
2024-08-13 13:08:34 +0900

    remove: 불필요한 코드 삭제

--- packages/client/src/apps/layout/ui/AuthLayout.tsx
+++ packages/client/src/apps/layout/ui/AuthLayout.tsx
@@ -4,7 +4,6 @@ import { COMMON_HEADER_META_DATA } from '@shared/model'
 import { usePathname } from 'next/navigation'
 import React, { useMemo } from 'react'
 import { Header, NavBar } from '@shared/ui/index'
-import { Button } from '@gds/component'
 
 interface Props {
    children: React.ReactNode
@@ -24,9 +23,6 @@ export function AuthLayout({ children }: Props) {
       <div>
          {!!headerTitle && <Header title={headerTitle} />}
          {children}
-         <Button variant="filled" size="medium">
-            Button
-         </Button>
          <NavBar />
          {/* TODO: fix NavBar to hide in some pages(기획 물어보기!!!) */}
       </div>
