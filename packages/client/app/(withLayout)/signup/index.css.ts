87fa527b1c0945ebe9bd3795f6f076207902d8b1
SuJin <kimsujin001127@gmail.com>
2024-08-16 23:46:40 +0900

    style: 상황별 상태 디자인 설정

--- packages/client/app/(withLayout)/signup/index.css.ts
+++ packages/client/app/(withLayout)/signup/index.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css'
+import { color } from '@gds/token'
 
 export const Wrapper = style({
    display: 'flex',
@@ -14,6 +15,8 @@ export const Title = style({
 export const InputBtnWrapper = style({
    display: 'flex',
    marginBottom: '16px',
+   fontWeight: '500',
+   fontSize: '14px',
 })
 
 export const NickNameBox = style({
@@ -36,6 +39,12 @@ export const DuplicatedBtnStyle = style({
 })
 
 export const NickNameDescriptionWrapper = style({
+   display: 'flex',
+   alignItems: 'center',
+})
+
+export const AcceptTxtStyle = style({
+   color: color['primary-main'],
    display: 'flex',
    alignItems: 'center',
 })
