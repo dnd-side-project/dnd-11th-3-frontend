18eec46d8f39fca58f315acaff702abfef90e728
SuJin <kimsujin001127@gmail.com>
2024-08-16 16:05:35 +0900

    remove: 불필요한코드 삭제

--- packages/client/app/layout.tsx
+++ packages/client/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
-import '@gmi-design-system/global.css'
 import localFont from 'next/font/local'
 import './design-system-globals.css'
 import ReactQueryClientProviders from '../src/apps/provider/reactQueryProviders'
