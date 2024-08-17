8591b6c4f7cff379a1d8c206cb1bd55370fde06c
doheeMac <thsehgml37@korea.ac.kr>
2024-08-17 22:18:29 +0900

    fix: fix build err

--- packages/client/src/design-system/token/typo/Pretendard.stories.tsx
+++ packages/client/src/design-system/token/typo/Pretendard.stories.tsx
@@ -1,4 +1,4 @@
-import { typo, typoBox, typoWrapper } from './typography.css'
+import { Typo as typo, typoBox, typoWrapper } from './typography.css'
 
 export default {
    title: 'Foundation/Pretendard',
@@ -12,21 +12,21 @@ export function Pretendard() {
             <div className={typoWrapper}>
                <div className={typoBox}>
                   <h3>h1</h3>
-                  <span className={typo.h1.Lg}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h1.Md}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h1.Sb}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h1.lg}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h1.md}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h1.sb}>공무원 업무습득! 공무인!</span>
                </div>
                <div className={typoBox}>
                   <h3>h2</h3>
-                  <span className={typo.h2.Lg}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h2.Md}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h2.Sb}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h2.lg}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h2.md}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h2.sb}>공무원 업무습득! 공무인!</span>
                </div>
                <div className={typoBox}>
                   <h3>h3</h3>
-                  <span className={typo.h3.Lg}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h3.Md}>공무원 업무습득! 공무인!</span>
-                  <span className={typo.h3.Sb}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h3.lg}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h3.md}>공무원 업무습득! 공무인!</span>
+                  <span className={typo.h3.sb}>공무원 업무습득! 공무인!</span>
                </div>
             </div>
          </div>
@@ -35,49 +35,49 @@ export function Pretendard() {
             <div className={typoWrapper}>
                <div className={typoBox}>
                   <h3>body1</h3>
-                  <span className={typo.body1.Lg}>
+                  <span className={typo.body1.lg}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body1.Md}>
+                  <span className={typo.body1.md}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body1.Sb}>
+                  <span className={typo.body1.sb}>
                      공무원 업무습득! 공무인!
                   </span>
                </div>
                <div className={typoBox}>
                   <h3>body2</h3>
-                  <span className={typo.body2.Lg}>
+                  <span className={typo.body2.lg}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body2.Md}>
+                  <span className={typo.body2.md}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body2.Sb}>
+                  <span className={typo.body2.sb}>
                      공무원 업무습득! 공무인!
                   </span>
                </div>
                <div className={typoBox}>
                   <h3>body3</h3>
-                  <span className={typo.body3.Lg}>
+                  <span className={typo.body3.lg}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body3.Md}>
+                  <span className={typo.body3.md}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body3.Sb}>
+                  <span className={typo.body3.sb}>
                      공무원 업무습득! 공무인!
                   </span>
                </div>
                <div className={typoBox}>
                   <h3>body4</h3>
-                  <span className={typo.body4.Lg}>
+                  <span className={typo.body4.lg}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body4.Md}>
+                  <span className={typo.body4.md}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.body4.Sb}>
+                  <span className={typo.body4.sb}>
                      공무원 업무습득! 공무인!
                   </span>
                </div>
@@ -88,13 +88,13 @@ export function Pretendard() {
             <div className={typoWrapper}>
                <div className={typoBox}>
                   <h3>hightlight1</h3>
-                  <span className={typo.highlight1.Lg}>
+                  <span className={typo.highlight1.lg}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.highlight1.Md}>
+                  <span className={typo.highlight1.md}>
                      공무원 업무습득! 공무인!
                   </span>
-                  <span className={typo.highlight1.Sb}>
+                  <span className={typo.highlight1.sb}>
                      공무원 업무습득! 공무인!
                   </span>
                </div>
