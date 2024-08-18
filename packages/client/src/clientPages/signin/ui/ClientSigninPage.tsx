758cad33daf1ac2788aef9c76a2bede7d8867ed9
doheeMac <thsehgml37@korea.ac.kr>
2024-08-18 21:17:54 +0900

    feat: add alert

--- packages/client/src/clientPages/signin/ui/ClientSigninPage.tsx
+++ packages/client/src/clientPages/signin/ui/ClientSigninPage.tsx
@@ -18,7 +18,14 @@ export function ClientSigninPage() {
                <SNSButton socialLoginType="kakao">카카오로 시작하기</SNSButton>
             </div>
             <div style={{ marginTop: '16px' }}>
-               <SNSButton socialLoginType="naver">네이버로 시작하기</SNSButton>
+               <SNSButton
+                  socialLoginType="naver"
+                  onClick={() =>
+                     alert('네이버 소셜 로그인 서비스는 준비 중입니다.')
+                  }
+               >
+                  네이버로 시작하기
+               </SNSButton>
             </div>
          </div>
       </div>
