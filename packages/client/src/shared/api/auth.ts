9da624ae02e5abd67b1fb39974cc466c266a7cc5
doheeMac <thsehgml37@korea.ac.kr>
2024-08-21 02:42:31 +0900

    feat: add auth

--- packages/client/src/shared/api/auth.ts
+++ packages/client/src/shared/api/auth.ts
@@ -10,21 +10,8 @@ import {
    useQueryClient,
    UseQueryOptions,
 } from '@tanstack/react-query'
-import { get } from 'http'
-import { Configuration } from '@server-api/configuration'
+
 import { config } from './config'
-
-// export const usePostTmpAccessToken = () =>
-//    useMutation({
-//       mutationFn: async (dto: TempLoginRequest) =>
-//          (
-//             await new SocialLoginAPIApi(unAuthConfig).getTempToken({
-//                ...dto,
-//             })
-//          ).data,
-
-//       gcTime: 1000 * 60 * 60, // 1hr
-//    })
 
 export const PostTmpAccessTokenQueryKey = '/signin/temporary/token'
 
@@ -38,14 +25,6 @@ export const usePostNicknameDuplCheck = () => {
          ).data,
    })
 }
-
-// export const useFetchKakaoSininRedirect = (options: UseQueryOptions) =>
-//    useQuery({
-//       ...options,
-//       queryKey: ['/auth/sigin/kakao'],
-//       queryFn: async () =>
-//          (await new SocialLoginAPIApi(unAuthConfig).()).data,
-//    })
 
 export const usePostLogout = () => {
    const queryClient = useQueryClient()
@@ -63,30 +42,6 @@ export const usePostReissueToken = () => {
          (await new SocialLoginAPIApi(config).reissue()).data,
    })
 }
-
-// export const usePostSignup = () => {
-//    const config = getTmpAuthorizedConfig()
-//    useMutation({
-//       mutationFn: async (dto: AdditionalInfoRequest) =>
-//          (
-//             await new SocialLoginAPIApi(config).signUp({
-//                ...dto,
-//             })
-//          ).data,
-//    })
-// }
-
-// export const usePostTempSignup = () => {
-//    const config = getTmpAuthorizedConfig()
-//    useMutation({
-//       mutationFn: async (dto: TempSignUpRequest) =>
-//          (
-//             await new SocialLoginAPIApi(config).tempSignUp({
-//                ...dto,
-//             })
-//          ).data,
-//    })
-// }
 
 export const usePostTempSignup = (
    dto: TempSignUpRequest,
