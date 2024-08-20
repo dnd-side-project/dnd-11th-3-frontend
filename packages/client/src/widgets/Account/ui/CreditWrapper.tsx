fda5285a6acdf9b2681b2960b3e7c5c5ba258a13
SuJin <kimsujin001127@gmail.com>
2024-08-20 18:37:33 +0900

    chore: 주석 추가

--- packages/client/src/widgets/Account/ui/CreditWrapper.tsx
+++ packages/client/src/widgets/Account/ui/CreditWrapper.tsx
@@ -4,12 +4,14 @@ import { color } from '@gds/token'
 import * as styles from './style.css'
 
 function CreditWrapper() {
+   // TODO: api 연결 필요
    return (
       <div className={styles.CreditBox}>
          <span className={styles.creditTitle}>크레딧</span>
          <hr className={styles.Line} />
          <div className={styles.creditBottomWrapper}>
             <div className={styles.AmountBox}>
+               {/** TODO: 콤마함수 적용하기 */}
                <span className={styles.creditAmount}>10000</span>
                <IconCreditFill color={color['primary-main']} />
             </div>
