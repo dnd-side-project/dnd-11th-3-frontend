a47c239347c54e47d57e692872d8f2d5d0b303aa
SuJin <kimsujin001127@gmail.com>
2024-08-21 14:39:41 +0900

    feat: nav icon 추가

--- packages/client/src/shared/ui/NavBar/NavBar.tsx
+++ packages/client/src/shared/ui/NavBar/NavBar.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { PageURL } from '@shared/model'
+import { IconHome, IconHuman, IconNavbarChat } from '@gds/icon'
+import { color } from '@gds/token'
 import { navbarWrapperStyle } from './navbar.css'
 
 export function NavBar() {
    const router = useRouter()
+   const currentUrl = usePathname()
 
    return (
       <div className={navbarWrapperStyle}>
@@ -14,6 +17,14 @@ export function NavBar() {
             onClick={() => router.push(PageURL.HOME)}
             aria-label="홈"
          >
+            <IconHome
+               size={32}
+               color={
+                  currentUrl === PageURL.HOME
+                     ? color['gray-200']
+                     : color['gray-400']
+               }
+            />
             Q&A
          </button>
          <button
@@ -21,6 +32,14 @@ export function NavBar() {
             onClick={() => router.push(PageURL.CHAT)}
             aria-label="채팅"
          >
+            <IconNavbarChat
+               size={32}
+               color={
+                  currentUrl === PageURL.CHAT
+                     ? color['gray-200']
+                     : color['gray-400']
+               }
+            />
             대화
          </button>
          <button
@@ -28,6 +47,14 @@ export function NavBar() {
             onClick={() => router.push(PageURL.ACCOUNT)}
             aria-label="마이페이지"
          >
+            <IconHuman
+               size={32}
+               color={
+                  currentUrl === PageURL.ACCOUNT
+                     ? color['gray-200']
+                     : color['gray-400']
+               }
+            />
             마이페이지
          </button>
       </div>
