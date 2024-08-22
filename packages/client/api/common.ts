c788ce83907e2c00d2e1909f84d600ea44d595b6
김수진 <kimsujin001127@gmail.com>
2024-08-22 18:12:44 +0900

    remove: 기존 코드로 수정 (#87)

--- packages/client/api/common.ts
+++ packages/client/api/common.ts
@@ -197,7 +197,6 @@ export const createRequestFunction = function (
             (axios.defaults.baseURL
                ? ''
                : (configuration?.basePath ?? basePath)) + axiosArgs.url,
-         withCredentials: true,
       }
       return axios.request<T, R>(axiosRequestArgs)
    }
