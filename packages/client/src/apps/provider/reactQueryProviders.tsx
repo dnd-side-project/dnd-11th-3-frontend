b18aa032b0865b16345bb6595ff6e2d31081b8b8
doheeMac <thsehgml37@korea.ac.kr>
2024-08-24 02:06:21 +0900

    fix: fix for buid

--- packages/client/src/apps/provider/reactQueryProviders.tsx
+++ packages/client/src/apps/provider/reactQueryProviders.tsx
@@ -24,7 +24,6 @@ export default function ReactQueryClientProviders({
                      }
                      return false
                   },
-                  
                },
             },
          }),
