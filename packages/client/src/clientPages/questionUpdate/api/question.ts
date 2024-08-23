800af403e97b3882e41f656d9dfc8d65cbd86d87
doheeMac <thsehgml37@korea.ac.kr>
2024-08-24 02:09:08 +0900

    fix: fix prettier for build err

--- packages/client/src/clientPages/questionUpdate/api/question.ts
+++ packages/client/src/clientPages/questionUpdate/api/question.ts
@@ -1,43 +1,39 @@
-import { APIApi, UpdateQuestionPostRequest } from "@server-api/api";
-import { config } from "@shared/api";
-import {
-    useMutation,
-    useQuery,
-    useQueryClient,
-    UseQueryOptions,
- } from '@tanstack/react-query'
+import { APIApi, UpdateQuestionPostRequest } from '@server-api/api'
+import { config } from '@shared/api'
+import { useMutation, useQuery, UseQueryOptions } from '@tanstack/react-query'
 // /api/question-posts/{questionPostId}
-//getQuestionPostById
+// getQuestionPostById
 
+export const useFetchQuestionPost = (
+   dto: { questionPostId: number },
+   options: UseQueryOptions,
+) =>
+   useQuery({
+      ...options,
+      queryKey: [`/question-posts/${dto.questionPostId}`],
+      queryFn: async () =>
+         (await new APIApi(config).getQuestionPostById(dto.questionPostId))
+            .data,
+   })
 
-
- 
- export const useFetchQuestionPost = (
-    dto:{questionPostId:number},
-    options: UseQueryOptions,
- ) =>
-    useQuery({
-       ...options,
-       queryKey: [`/question-posts/${dto.questionPostId}`],
-       queryFn: async () =>
-          (
-             await new APIApi(config).getQuestionPostById(dto.questionPostId)
-          ).data,
-    })
-
-
-    /**
- * 
+/**
+ *
  * @description update question post
  */
-    export const usePatchQuestion = () => {
-        return useMutation({
-           mutationFn: async ({questionPostId, questionData}:{questionPostId:number,
-           questionData :UpdateQuestionPostRequest
-           }) =>
-              (await new APIApi(config).updateQuestionPosts(
-                questionPostId,
-                questionData
-              )).data,
-        })
-     }
+export const usePatchQuestion = () => {
+   return useMutation({
+      mutationFn: async ({
+         questionPostId,
+         questionData,
+      }: {
+         questionPostId: number
+         questionData: UpdateQuestionPostRequest
+      }) =>
+         (
+            await new APIApi(config).updateQuestionPosts(
+               questionPostId,
+               questionData,
+            )
+         ).data,
+   })
+}
