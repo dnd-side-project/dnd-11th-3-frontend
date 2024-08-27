eb4615966dabd05087dacb08262758918059e423
SuJin <kimsujin001127@gmail.com>
2024-08-27 17:38:37 +0900

    feat: multiSelect 추가

--- packages/client/src/clientPages/search/ui/ClientSearchPage.tsx
+++ packages/client/src/clientPages/search/ui/ClientSearchPage.tsx
@@ -1,10 +1,44 @@
+'use client'
+
 import { SearchHeader } from '@widgets/Search'
+import {
+   MultiSelect,
+   SelectItemType,
+} from 'src/design-system/component/MultiSelect'
+import { IconFilter } from '@gds/icon'
+import { useState } from 'react'
+import { JOB_GROUPS } from '@shared/model/job'
 import * as styles from './style.css'
 
 export function ClientSearchPage() {
+   const [selectedJobGroups, setSelectedJobGroups] = useState<SelectItemType[]>(
+      [],
+   )
+
+   const handleSelect = (selectedItems: SelectItemType[]) => {
+      if (selectedItems.length <= 3) {
+         setSelectedJobGroups(selectedItems)
+      }
+   }
+
    return (
       <div className={styles.Container}>
          <SearchHeader />
+         <div className={styles.MulitSelectBox}>
+            <MultiSelect
+               variant="default"
+               inputProps={{
+                  icon: <IconFilter />,
+               }}
+               onSelect={handleSelect}
+               selected={selectedJobGroups}
+               placeholder="직군 3개까지 선택 가능합니다."
+               items={JOB_GROUPS.map((jobGroup) => ({
+                  id: jobGroup,
+                  label: jobGroup,
+               }))}
+            />
+         </div>
       </div>
    )
 }
--- packages/client/src/clientPages/search/ui/style.css.ts
+++ packages/client/src/clientPages/search/ui/style.css.ts
@@ -4,3 +4,9 @@ export const Container = style({
    display: 'flex',
    flexDirection: 'column',
 })
+
+export const MulitSelectBox = style({
+   display: 'flex',
+   justifyContent: 'center',
+   marginTop: '-57px',
+})
