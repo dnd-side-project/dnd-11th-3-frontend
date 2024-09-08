7d023c6813bebfc0195096f7e8bc74d290848325
SuJin <kimsujin001127@gmail.com>
2024-09-08 20:36:50 +0900

    feat: error 처리

--- packages/client/src/clientPages/search/ui/ClientSearchPage.tsx
+++ packages/client/src/clientPages/search/ui/ClientSearchPage.tsx
@@ -11,6 +11,7 @@ import { JOB_GROUPS } from '@shared/model/job'
 import QuestionCard from '@shared/ui/QuestionList/Question'
 import { MAX_SELECT_JOB_LENGTH } from '@shared/model'
 import { useFetchQuestions } from 'src/clientPages/home/api/question'
+import { useRouter } from 'next/navigation'
 import * as styles from './style.css'
 
 interface Prop {
@@ -19,10 +20,10 @@ interface Prop {
 
 export function ClientSearchPage({ input }: Prop) {
    const [searchInput, setSearchInput] = useState<string>(decodeURI(input))
-
    const [selectedJobGroups, setSelectedJobGroups] = useState<SelectItemType[]>(
       [],
    )
+   const router = useRouter()
 
    const handleSelect = (selectedItems: SelectItemType[]) => {
       if (selectedItems.length <= MAX_SELECT_JOB_LENGTH) {
@@ -30,18 +31,28 @@ export function ClientSearchPage({ input }: Prop) {
       }
    }
 
-   const { data: questionsData, status: questionListsStatus } =
-      useFetchQuestions({
-         condition: {
-            keyword: decodeURI(input),
-            jobGroups: selectedJobGroups.map((jobGroup) => jobGroup.label),
-            isChosen: false,
-         },
-         pageable: {
-            page: 0,
-            size: 100,
-         },
-      })
+   const {
+      data: searchQuestionsData,
+      status: searchQuestionsDataStatus,
+      isError: searchQuestionsDataIsError,
+      error: searchQuestionsDataError,
+   } = useFetchQuestions({
+      condition: {
+         keyword: decodeURI(input),
+         jobGroups: selectedJobGroups.map((jobGroup) => jobGroup.label),
+         isChosen: false,
+      },
+      pageable: {
+         page: 0,
+         size: 100,
+      },
+   })
+
+   if (searchQuestionsDataIsError) {
+      router.push('/home')
+      // TODO: toast로 수정 필요
+      alert(searchQuestionsDataError.message || '서버 오류가 발생했습니다.')
+   }
 
    return (
       <div className={styles.Container}>
@@ -62,10 +73,10 @@ export function ClientSearchPage({ input }: Prop) {
             />
          </div>
          <div className={styles.SearchCountBox}>
-            <span>검색결과 {questionsData?.content?.length}건</span>
+            <span>검색결과 {searchQuestionsData?.content?.length}건</span>
          </div>
          <div className={styles.QuestionContainer}>
-            {questionsData?.content?.map((question) => {
+            {searchQuestionsData?.content?.map((question) => {
                return (
                   <QuestionCard data={question} key={question.questionPostId} />
                )
