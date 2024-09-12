5dc4075da279ff2a1e4bf3feb40bdce80ae40dd5
SuJin <kimsujin001127@gmail.com>
2024-09-12 18:15:45 +0900

    feat: /account/credit 크레딧 api 연결

--- packages/client/src/widgets/AccountCredit/ui/CreditAmount.tsx
+++ packages/client/src/widgets/AccountCredit/ui/CreditAmount.tsx
@@ -5,14 +5,18 @@ import { formatNumberWithCommas } from '@shared/utils/formatNumberWithCommas'
 import CreditBackgroundImg from 'public/asset/creditBackground.svg'
 import * as styles from './style.css'
 
-function CreditAmount() {
+interface Prop {
+   creditAmount?: number
+}
+
+function CreditAmount({ creditAmount }: Prop) {
    return (
       <div className={styles.CreditBackground}>
          <Image src={CreditBackgroundImg} alt="credit background" />
          <div className={styles.CreditBox}>
             <span className={styles.CreditTxt}>크레딧</span>
             <div className={styles.CreditAmountBox}>
-               {formatNumberWithCommas(10000)}
+               {formatNumberWithCommas(creditAmount || 0)}
                <div className={styles.CreditIconBox}>
                   <IconCredit color={color['primary-main']} />
                </div>
