59b845597948bf50a35dbb6d83cd505094666860
SuJin <kimsujin001127@gmail.com>
2024-09-15 00:31:04 +0900

    chore: 불필요한 코드 삭제

--- packages/client/src/widgets/ChatDetail/ui/ReceiveMessage.tsx
+++ packages/client/src/widgets/ChatDetail/ui/ReceiveMessage.tsx
@@ -3,7 +3,7 @@ import { messageBox, timeTxt } from './style.css'
 function ReceiveMessage() {
    return (
       <>
-         <div className={messageBox({ type: 'receive' })}>받은메세지</div>{' '}
+         <div className={messageBox({ type: 'receive' })}>받은메세지</div>
          <div>
             <div className={timeTxt}>16:20</div>
          </div>
